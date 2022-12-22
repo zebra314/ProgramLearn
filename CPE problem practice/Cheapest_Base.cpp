f2c4f7b3feb124c2b503c932fe9b5b0aac1184c0
alfonso314159 <kidalfonso@gmail.com>
2022-12-22 19:09:10 +0800

    GPE

--- CPE problem practice/Cheapest_Base.cpp
+++ CPE problem practice/Cheapest_Base.cpp
@@ -1,38 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void Find_cheapest_set(long long int num, vector<int> cost){
+    int base, price; // the copy of num
+    long long int coco;
+    map<int, set<int>> price2base; // key->price, value->base
+    set<int> coprice; // the base with same price
+    cout<<"Cheapest base(s) for number "<<num<<":";
+    int cheapest ;
+    for(int j = 2; j<=36; j++){ // base
+        price = 0;
+        coco = num;
+        while(coco!=0){
+            price += cost[coco%j];
+            coco /= j;
+        }
+        price2base[price].insert(j);
+        if(j == 2){
+            cheapest = price;
+        }
+        else if(price < cheapest) {
+            cheapest = price;
+        }
+    }
+    for(const auto &ele : price2base[cheapest])
+        cout<<" "<<ele;
+    cout<<'\n';
+}
 int main(){
-    int case_1, case_2, price, ans;
-    int cheapest_price, cheapest;
+ 
+    int case_1, case_2, price;
+    int cheapest;
+    long long int num;
     vector<int> cost;
     cin>>case_1;
     int i = 0;
     while(i<case_1 and ++i){
-        cout<<"Case "<<i<<": \n";
+        cost.clear();
+        if(i>1) cout<<'\n';
+        cout<<"Case "<<i<<":"<<'\n';
         for(int j = 0;j<36; j++){
             cin>>price;
             cost.push_back(price);
         }
         cin>>case_2;
-        int num;
-        int base;
-        int coco; // the copy of num
         while(case_2--){
             cin>>num;
-            cheapest = 1000000;
-            for(int j = 2; j<=36; j++){
-                price = 0;
-                coco = num;
-                while(coco!=0){
-                    price += cost[coco%j];
-                    coco /= j;
-                }
-                if(price < cheapest) {
-                    cheapest = price;
-                    base = j;
-                }
-            }
-            cout<<base<<'\n';
+            Find_cheapest_set(num, cost);
         }
     }
 }
