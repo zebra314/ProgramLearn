1fd2e084fd98e1283d3717e22ac70103511a5735
zebra314 <kidalfonso@gmail.com>
2023-03-07 02:39:02 +0800

    Tweak architecture

---
+++ intro_to_ai/HW1/AI_HW1/dataset.py
@@ -0,0 +1,17 @@
+import os
+import cv2
+
+def loadImages(dataPath):
+    """
+    load all Images in the folder and transfer a list of tuples. The first 
+    element is the numpy array of shape (m, n) representing the image. 
+    The second element is its classification (1 or 0)
+      Parameters:
+        dataPath: The folder path.
+      Returns:
+        dataset: The list of tuples.
+    """
+    # Begin your code (Part 1)
+    raise NotImplementedError("To be implemented")
+    # End your code (Part 1)
+    return dataset
